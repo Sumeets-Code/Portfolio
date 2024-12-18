64887bb9a6aaf61ddf243dc95374a398afd2fc59
Sumeet bhagat <sumeetbhagat1811@gmail.com>
2024-12-19 01:51:13 +0530

    Added new files needed, created server, edited css file for background images

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3400;
+const port = 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req ,res) => {
+    res.render("index");
+})
+
+app.get("/hobbies", (req ,res) => {
+    res.render("hobbies");
+})
+
+app.get("/contact", (req ,res) => {
+    res.render("contact");
+})
 
 app.listen (port, () => {
     console.log(`Server is running on port ${port}`);
