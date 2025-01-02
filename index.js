55e0e6cee53ed3ba5ad870d6d38a6b4dd069ec8b
Sumeet bhagat <sumeetbhagat1811@gmail.com>
2025-01-02 19:20:43 +0530

    Added My work Page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from 'url';
 
@@ -8,6 +9,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req ,res) => {
     res.render("index");
@@ -19,6 +22,10 @@ app.get("/hobbies", (req ,res) => {
 
 app.get("/contact", (req ,res) => {
     res.render("contact");
+})
+
+app.get("/mywork", (req ,res) => {
+    res.render("myWork");
 })
 
 app.listen (port, () => {
